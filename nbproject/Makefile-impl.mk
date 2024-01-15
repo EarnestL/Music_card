c2dcb5d85697ab601e819716a3f1214b84f141de
EarnestL <earnest.lin.2002@gmail.com>
2024-01-14 22:39:26 -0800

    build

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Music_card.X
+PROJECTNAME=Music_card
 
 # Active Configuration
 DEFAULTCONF=default
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=Music_card.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/Music_card.X.production.hex
+CND_ARTIFACT_NAME_default=Music_card.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Music_card.production.hex
